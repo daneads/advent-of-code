7716abd2d247d050f9ab9d185bd951f6231eb6cf
Dan Eads <24708079+daneads@users.noreply.github.com>
2024-12-01 16:07:32 -0500

    Add 2023 day 2 part 1

--- src/Solutions/Year2023.hs
+++ src/Solutions/Year2023.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Solutions.Year2023 (day1Part1, day1Part2, getIntsForLine, getValFromInts, solutions) where
+module Solutions.Year2023 (day1Part1, day1Part2, day2Part1, getIntsForLine, getValFromInts, solutions) where
 
 import Data.Char (digitToInt, isDigit)
+import qualified Data.HashMap.Strict as HM
 import Data.List (foldl')
 import Data.Maybe (mapMaybe)
 import qualified Data.Text as T
@@ -39,5 +40,41 @@ day1Part2 xs =
   let allVals = fmap (getValFromInts . getIntsForLine . convertDigitStringsToInts) xs
    in show $ sum allVals
 
+parseColor :: T.Text -> (T.Text, Int)
+parseColor s =
+  let s' = T.words s
+   in (last s', read . T.unpack $ head s')
+
+maxColors :: HM.HashMap T.Text Int
+maxColors = HM.fromList [("red", 12), ("green", 13), ("blue", 14)]
+
+colorIsValid :: (T.Text, Int) -> Bool
+colorIsValid (color, count) = count <= HM.findWithDefault 0 color maxColors
+
+setIsValid :: HM.HashMap T.Text Int -> Bool
+setIsValid set = all colorIsValid $ HM.toList set
+
+parseSet :: T.Text -> HM.HashMap T.Text Int
+parseSet s =
+  let colorsRaw = T.splitOn ", " s
+      colors = fmap parseColor colorsRaw
+   in HM.fromList colors
+
+parseGame :: String -> (Int, [HM.HashMap T.Text Int])
+parseGame s =
+  let s' = T.splitOn ": " (T.pack s)
+      rawGame = T.words $ head s'
+      gameId = read . T.unpack $ last rawGame
+      rawSets = T.splitOn "; " $ last s'
+      sets = fmap parseSet rawSets
+   in (gameId, sets)
+
+day2Part1 :: [String] -> String
+day2Part1 xs =
+  let games = fmap parseGame xs
+      validGames = filter (\(_, game) -> all setIsValid game) games
+      gameIdSum = sum $ fmap fst validGames
+   in show gameIdSum
+
 solutions :: [Solution]
-solutions = [Solution 2023 1 1 day1Part1, Solution 2023 1 2 day1Part2]
+solutions = [Solution 2023 1 1 day1Part1, Solution 2023 1 2 day1Part2, Solution 2023 2 1 day2Part1]
--- test/Year2023.hs
+++ test/Year2023.hs
@@ -13,5 +13,18 @@ testsDayOne =
       testday1Part2Sample = TestCase (assertEqual "Day 1 Part 2 Sample" "281" day1Part2Solution)
    in TestList [testDayOneGetIntsForLineOneInt, testDayOneGetIntsForLineTwoInts, testday1Part1Sample, testday1Part2Sample]
 
+testsDayTwo :: Test
+testsDayTwo =
+  let day2Part1Solution =
+        day2Part1
+          [ "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+          ]
+      testday2Part1Sample = TestCase (assertEqual "Day 2 Part 1 Sample" "8" day2Part1Solution)
+   in TestList [testday2Part1Sample]
+
 tests2023 :: Test
-tests2023 = TestList [testsDayOne]
+tests2023 = TestList [testsDayOne, testsDayTwo]
